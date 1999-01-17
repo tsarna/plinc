9cfa617b40a29bb30ad8e18436ba0e31c4a91e26
Ty Sarna <ty@sarna.org>
1999-01-17 22:30:04 +0000

    Initial revision
    git-svn-id: file:///Users/tsarna/src/SVNREPO/plinc/trunk@21 933abd37-16e6-440f-98ae-168aefc19175

---
+++ arith.c
@@ -0,0 +1,96 @@
+/* $Endicor: stack.c,v 1.4 1999/01/17 21:06:23 tsarna Exp tsarna $ */
+
+#include <plinc/interp.h>
+
+#include <stdlib.h>
+
+
+
+#ifdef notyet
+static void *
+op_sub(PlincInterp *i)
+{
+    PlincVal *v1, *v2, v;
+    int t1, t2;
+    float f2;
+    
+    if (!PLINC_OPSTACKHAS(i, 2)) {
+        return i->stackunderflow;
+    } else if (!PLINC_OPSTACKROOM(i, 1)) {
+        return i->stackoverflow;
+    } else {
+        v1 = &PLINC_OPTOPDOWN(i, 0);
+        v2 = &PLINC_OPTOPDOWN(i, 1);
+
+        t1 = PLINC_TYPE(*v1);
+        t2 = PLINC_TYPE(*v2);
+    
+        if (t1 == PLINC_TYPE_INT) {
+            if (t2 == PLINC_TYPE_INT) {
+            } else if (t2 == PLINC_TYPE_REAL) {
+            } else {
+                return i->typecheck;
+            }
+        } else if (t1 == PLINC_TYPE_REAL) {
+            f2 = v2->Val.Real;
+            
+            if (t2 == PLINC_TYPE_INT) {
+                /* cast to real */
+                
+                f2 = (float)(v2->Val.Int;
+                t2 = PLINC_TYPE_REAL;
+            }
+
+            if (t2 == PLINC_TYPE_REAL) {
+                
+            } else {
+                return i->typecheck;
+            }
+        } else {
+            return i->typecheck;
+        }
+    }
+}
+#endif
+
+
+
+static void *
+op_neg(PlincInterp *i)
+{
+    PlincVal *v;
+    
+    if (!PLINC_OPSTACKHAS(i, 1)) {
+        return i->stackunderflow;
+    } else {
+        v = &PLINC_OPTOPDOWN(i, 0);
+        
+        if (PLINC_TYPE(*v) == PLINC_TYPE_INT) {
+            v->Val.Int = -(v->Val.Int);
+#ifdef WITH_REAL
+        } if (PLINC_TYPE(*v) == PLINC_TYPE_REAL) {
+            v->Val.Real = -(v->Val.Real);
+#endif
+        } else {
+            return i->typecheck;
+        }
+    }
+
+    return NULL;
+}
+
+
+
+static PlincOp ops[] = {
+    {"neg",         op_neg},
+
+    {NULL,          NULL}
+};
+
+
+
+void
+PlincInitArithOps(PlincInterp *i)
+{
+    PlincInitOps(i, ops);
+}
