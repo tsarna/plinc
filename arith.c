c3773b3a192bafdbdc13839f57ffc7cc06312b46
Ty Sarna <ty@sarna.org>
1999-01-22 02:10:37 +0000

    add/sub
    git-svn-id: file:///Users/tsarna/src/SVNREPO/plinc/trunk@44 933abd37-16e6-440f-98ae-168aefc19175

--- arith.c
+++ arith.c
@@ -1,4 +1,4 @@
-/* $Endicor: arith.c,v 1.4 1999/01/20 20:30:12 tsarna Exp tsarna $ */
+/* $Endicor: arith.c,v 1.5 1999/01/20 23:14:23 tsarna Exp $ */
 
 #include <plinc/interp.h>
 
@@ -60,52 +60,101 @@ op_mod(PlincInterp *i)
 
 
 
-#ifdef notyet
 static void *
-op_sub(PlincInterp *i)
+addsub(PlincInterp *i, int add)
 {
     PlincVal *v1, *v2, v;
     int t1, t2;
-    float f2;
 
     if (!PLINC_OPSTACKHAS(i, 2)) {
         return i->stackunderflow;
-    } else if (!PLINC_OPSTACKROOM(i, 1)) {
-        return i->stackoverflow;
     } else {
-        v1 = &PLINC_OPTOPDOWN(i, 0);
-        v2 = &PLINC_OPTOPDOWN(i, 1);
+        v1 = &PLINC_OPTOPDOWN(i, 1);
+        v2 = &PLINC_OPTOPDOWN(i, 0);
 
         t1 = PLINC_TYPE(*v1);
         t2 = PLINC_TYPE(*v2);
 
-        if (t1 == PLINC_TYPE_INT) {
-            if (t2 == PLINC_TYPE_INT) {
-            } else if (t2 == PLINC_TYPE_REAL) {
+        if ((t1 == PLINC_TYPE_INT) && (t2 == PLINC_TYPE_INT)) {
+            int x, y, a, b;
+            
+            a = v1->Val.Int;
+            b = v2->Val.Int;
+            x = a + b;
+            y = a - b;
+            
+            if ((add && ((x^a) < 0 && (x^b) < 0))
+            ||  (!add && ((y^a) < 0 && (y^~b) < 0))) {
+#ifdef WITH_REAL
+                /* it would overflow, so convert to reals */
+
+                v1->Val.Real = (float)(v1->Val.Int);
+                v2->Val.Real = (float)(v2->Val.Int);
+
+                t1 = t2 = PLINC_TYPE_REAL;
+#else
+                return i->rangecheck;
+#endif
             } else {
-                return i->typecheck;
+                v.Flags = PLINC_TYPE_INT;
+                v.Val.Int = add ? x : y;
+
+                PLINC_OPPOP(i);
+                PLINC_OPPOP(i);
+                PLINC_OPPUSH(i, v);
+                
+                return NULL;
             }
-        } else if (t1 == PLINC_TYPE_REAL) {
-            f2 = v2->Val.Real;
-
-            if (t2 == PLINC_TYPE_INT) {
-                /* cast to real */
-
-                f2 = (float)(v2->Val.Int;
-                t2 = PLINC_TYPE_REAL;
-            }
-
-            if (t2 == PLINC_TYPE_REAL) {
-
-            } else {
-                return i->typecheck;
-            }
-        } else {
-            return i->typecheck;
         }
+        
+#ifdef WITH_REAL
+        if ((t1 == PLINC_TYPE_INT) && (t2 == PLINC_TYPE_REAL)) {
+                v1->Val.Real = (float)(v1->Val.Int);
+
+                t1 = PLINC_TYPE_REAL;
+        }
+
+        if ((t1 == PLINC_TYPE_REAL) && (t2 == PLINC_TYPE_INT)) {
+                v2->Val.Real = (float)(v2->Val.Int);
+
+                t2 = PLINC_TYPE_REAL;
+        }
+
+        if ((t1 == PLINC_TYPE_REAL) && (t2 == PLINC_TYPE_REAL)) {
+            v.Flags = PLINC_ATTR_LIT | PLINC_TYPE_REAL;
+            if (add) {
+                v.Val.Real = v1->Val.Real + v2->Val.Real;
+            } else {
+                v.Val.Real = v1->Val.Real - v2->Val.Real;
+            }
+
+            PLINC_OPPOP(i);
+            PLINC_OPPOP(i);
+            PLINC_OPPUSH(i, v);
+            
+            return NULL;
+        }
+#endif
+
+        return i->typecheck;
     }
 }
-#endif
+
+
+
+static void *
+op_add(PlincInterp *i)
+{
+    return addsub(i, TRUE);
+}
+
+
+
+static void *
+op_sub(PlincInterp *i)
+{
+    return addsub(i, FALSE);
+}
 
 
 
@@ -124,7 +173,7 @@ op_abs(PlincInterp *i)
                 v->Val.Int = -(v->Val.Int);
             }
 #ifdef WITH_REAL
-        } if (PLINC_TYPE(*v) == PLINC_TYPE_REAL) {
+        } else if (PLINC_TYPE(*v) == PLINC_TYPE_REAL) {
             v->Val.Real = fabsf(v->Val.Real);
 #endif
         } else {
@@ -150,7 +199,7 @@ op_neg(PlincInterp *i)
         if (PLINC_TYPE(*v) == PLINC_TYPE_INT) {
             v->Val.Int = -(v->Val.Int);
 #ifdef WITH_REAL
-        } if (PLINC_TYPE(*v) == PLINC_TYPE_REAL) {
+        } else if (PLINC_TYPE(*v) == PLINC_TYPE_REAL) {
             v->Val.Real = -(v->Val.Real);
 #endif
         } else {
@@ -164,9 +213,12 @@ op_neg(PlincInterp *i)
 
 
 static const PlincOp ops[] = {
+    {"add",         op_add},
+
     {"idiv",        op_idiv},
     {"mod",         op_mod},
 
+    {"sub",         op_sub},
     {"abs",         op_abs},
     {"neg",         op_neg},
 
