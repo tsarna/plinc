cae2d2d9650445aeee4c8e40332dcc525ee5c43b
Ty Sarna <ty@sarna.org>
2004-02-18 03:09:33 +0000

    anchorsearch, and make version return a const string
    git-svn-id: file:///Users/tsarna/src/SVNREPO/plinc/trunk@110 933abd37-16e6-440f-98ae-168aefc19175

--- string.c
+++ string.c
@@ -70,6 +70,66 @@ op_string(PlincInterp *i)
 }
 
 
+                
+static void *
+op_anchorsearch(PlincInterp *i)
+{
+    PlincVal *v1, *v2, nv;
+    PlincUInt32 l1, l2;
+    char *p1;
+        
+    if (!PLINC_OPSTACKHAS(i, 2)) {
+        return i->stackunderflow;
+    } else if (!PLINC_OPSTACKROOM(i, 1)) {
+        return i->stackoverflow;
+    } else {
+        v1 = &PLINC_OPTOPDOWN(i, 1);
+        v2 = &PLINC_OPTOPDOWN(i, 0);
+        
+        if ((PLINC_TYPE(*v1) != PLINC_TYPE_STRING)
+        &&  (PLINC_TYPE(*v2) != PLINC_TYPE_STRING)) {
+            return i->typecheck;
+        } else if (!PLINC_CAN_READ(*v1) || !PLINC_CAN_READ(*v2)){
+            return i->invalidaccess;
+        } else {
+            p1 = v1->Val.Ptr;
+            l1 = PLINC_SIZE(*v1);
+            l2 = PLINC_SIZE(*v2);
+
+            if (l1 >= l2) {
+                if (!memcmp(p1, v2->Val.Ptr, l2)) {
+                    PLINC_OPPOP(i);
+                    PLINC_OPPOP(i);
+                
+                    nv.Flags = PLINC_ATTR_LIT | PLINC_TYPE_STRING | (l1 - l2);
+                    nv.Val.Ptr = (p1 + l2);
+                    PLINC_OPPUSH(i, nv);
+
+                    nv.Flags = PLINC_ATTR_LIT | PLINC_TYPE_STRING | (l2);
+                    nv.Val.Ptr = p1;
+                    PLINC_OPPUSH(i, nv);
+
+                    nv.Flags = PLINC_ATTR_LIT | PLINC_TYPE_BOOL;
+                    nv.Val.Int = TRUE;
+                    PLINC_OPPUSH(i, nv);    
+
+                    return NULL;
+                }
+            }
+            
+            PLINC_OPPOP(i);
+
+            nv.Flags = PLINC_ATTR_LIT | PLINC_TYPE_BOOL;
+            nv.Val.Int = FALSE;
+                    
+            PLINC_OPPUSH(i, nv);
+        }
+    }
+
+    return NULL;
+}
+
+
 
 static void *
 op_cvn(PlincInterp *i)
@@ -110,28 +170,25 @@ op_version(PlincInterp *i)
     if (!PLINC_OPSTACKROOM(i, 1)) {
         return i->stackoverflow;
     } else {
-        nv.Flags = PLINC_ATTR_LIT | PLINC_TYPE_STRING | strlen(PLINC_VERSION);
-        nv.Val.Ptr = PlincNewString(i->Heap, strlen(PLINC_VERSION));
-        
-        if (nv.Val.Ptr) {   
-            memcpy(nv.Val.Ptr, PLINC_VERSION, strlen(PLINC_VERSION));
-            PLINC_OPPUSH(i, nv);
+        nv.Flags = PLINC_ATTR_LIT | PLINC_TYPE_STRING | PLINC_ATTR_NOWRITE \
+            | strlen(PLINC_VERSION);
+        nv.Val.Ptr = PLINC_VERSION;
 
-            return NULL;
-        } else {
-            return i->VMerror;
-        }
+        PLINC_OPPUSH(i, nv);
     }
+
+    return NULL;
 }
 
 
 
 static const PlincOp ops[] = {
-    {op_string,     "string"},
-    {op_cvn,        "cvn"},
-    {op_version,    "version"},
+    {op_string,         "string"},
+    {op_anchorsearch,   "anchorsearch"},
+    {op_cvn,            "cvn"},
+    {op_version,        "version"},
 
-    {NULL,          NULL}
+    {NULL,              NULL}
 };
 
 
