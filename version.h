51df37b9952cbe9e2468b4fbaceff7891b0a20c1
Ty Sarna <ty@sarna.org>
2004-02-28 19:17:56 +0000

    split flush/reset into 4 file methods, and implemented flt_str
    git-svn-id: file:///Users/tsarna/src/SVNREPO/plinc/trunk@195 933abd37-16e6-440f-98ae-168aefc19175

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define PLINC_VERSION_H
 
 #define PLINC_PRODUCT "Plinc"
-#define PLINC_VERSION "Milestone 16"
-#define PLINC_REVISION  (-100+16)
+#define PLINC_VERSION "Milestone 17"
+#define PLINC_REVISION  (-100+17)
 
 #endif /* PLINC_VERSION_H */
