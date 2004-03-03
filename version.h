c9532657a63b7dae0b0056aa2edc25963ce0e3af
Ty Sarna <ty@sarna.org>
2004-03-03 17:05:19 +0000

    readhexstring and hex decode filter
    git-svn-id: file:///Users/tsarna/src/SVNREPO/plinc/trunk@202 933abd37-16e6-440f-98ae-168aefc19175

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define PLINC_VERSION_H
 
 #define PLINC_PRODUCT "Plinc"
-#define PLINC_VERSION "Milestone 17"
-#define PLINC_REVISION  (-100+17)
+#define PLINC_VERSION "Milestone 18"
+#define PLINC_REVISION  (-100+18)
 
 #endif /* PLINC_VERSION_H */
