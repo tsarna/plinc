621587ec224e5836ab01f3f2b267545a8e7aa7f7
Ty Sarna <ty@sarna.org>
2004-03-08 22:49:52 +0000

    flt_pstr
    git-svn-id: file:///Users/tsarna/src/SVNREPO/plinc/trunk@218 933abd37-16e6-440f-98ae-168aefc19175

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define PLINC_VERSION_H
 
 #define PLINC_PRODUCT "Plinc"
-#define PLINC_VERSION "Milestone 18"
-#define PLINC_REVISION  (-100+18)
+#define PLINC_VERSION "Milestone 19"
+#define PLINC_REVISION  (-100+19)
 
 #endif /* PLINC_VERSION_H */
