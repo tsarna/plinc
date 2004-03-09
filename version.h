26cb6b6ff63de9ec83f988c1b18ffacff889eae5
Ty Sarna <ty@sarna.org>
2004-03-09 18:29:21 +0000

    new tokenizer being used
    git-svn-id: file:///Users/tsarna/src/SVNREPO/plinc/trunk@231 933abd37-16e6-440f-98ae-168aefc19175

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define PLINC_VERSION_H
 
 #define PLINC_PRODUCT "Plinc"
-#define PLINC_VERSION "Milestone 19"
-#define PLINC_REVISION  (-100+19)
+#define PLINC_VERSION "Milestone 20"
+#define PLINC_REVISION  (-100+20)
 
 #endif /* PLINC_VERSION_H */
