0ef9bd33dd099307bcc3f43f4df17fc26ce93ad6
Ty Sarna <ty@sarna.org>
2004-03-10 18:42:56 +0000

    kill old tokenizer
    git-svn-id: file:///Users/tsarna/src/SVNREPO/plinc/trunk@239 933abd37-16e6-440f-98ae-168aefc19175

--- defs.py
+++ defs.py
@@ -4,20 +4,6 @@
 early = r"""
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% EARLY DEFINITIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% The odd style here is a result of the fact that it's early in
-% interpreter initialization, and we have only the C-implemented
-% operators available.  In fact, the point here is to define
-% the operators ususally used in defining stuff.
-
-% define the '}' operator
-
-(#) [
-    /.decscan load .doexec
-    (]) load .doexec
-    /cvx load .doexec
-] .doexec def
-
-
 % define the 'bind' operator
 
 /bind {} dup exec def
--- token.c
+++ token.c
@@ -6,343 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#if 0
-#define WS  0x8000      /* Whitespace */
-#define SP  0x4000      /* Special */
-#define SN  0x2000      /* Start Number */
-#define DG  0x1000      /* Digit */
 
-#include "toktab.h"
-
-
-#define isspace(c)      (_toktab[(unsigned char)(c)] & WS)
-#define isspecial(c)    (_toktab[(unsigned char)(c)] & SP)
-#define isendname(c)    (_toktab[(unsigned char)(c)] & (SP|WS))
-#define isstartnum(c)   (_toktab[(unsigned char)(c)] & SN)
-#define isdigit(c)      (_toktab[(unsigned char)(c)] & DG)
-#define isoctal(c)      (isdigit(c) && ((c) < '8'))
-#define val(c)          (_toktab[(unsigned char)(c)] & 0xFF)
-
-
-static void    *NumToken(PlincInterp *i, char *buf, size_t len, size_t *eaten, PlincVal *v);
-static int      IntToken(char **p, size_t *l, PlincInt *n, int base);
-static void    *NameToken(PlincInterp *i, char *buf, size_t len, size_t *eaten, PlincVal *v);
-static void    *StringToken(PlincInterp *i, char *buf, size_t len, size_t *eaten, PlincVal *v);
-
-
-void *
-PlincToken(PlincInterp *i, char *buf, size_t len, size_t *eaten, PlincVal *v)
-{
-    char *p = buf;
-    size_t l = len;
-
-    *eaten = 0;
-
-    v->Flags = 0;
-
-    while (l) {
-        if (isspace(*p)) {
-            do {
-                p++; l--;
-            } while (l && isspace(*p));
-        } else if (*p == '%') {
-            /* eat everything up until a newline */
-            do {
-                p++; l--;
-            } while (l && (*p != '\n'));
-
-            if (l) {
-                /* eat the newline */
-                p++; l--;
-            }
-        } else if (*p == '[') {
-            v->Val.Ptr = i->LeftBracket;
-            goto ok1;
-        } else if (*p == ']') {
-            v->Val.Ptr = i->RightBracket;
-            goto ok1;
-        } else if (*p == '{') {
-            v->Val.Ptr = i->LeftBrace;
-            goto ok2;
-        } else if (*p == '}') {
-            v->Val.Ptr = i->RightBrace;
-            goto ok2;
-        } else if (*p == '>') {
-            p++; l--;
-            if (l) {
-                if  (*p == '>') {
-                    v->Val.Ptr = i->RightAngleAngle;
-                    goto ok1;
-                } else {
-                    return i->syntaxerror;
-                }
-            }
-        } else if (*p == '<') {
-            p++; l--;
-            if (l) {
-                if (*p == '<') {
-                    v->Val.Ptr = i->LeftAngleAngle;
-                    goto ok1;
-                }
-            }
-        } else if (*p == '(') {
-            p++; l--;
-            *eaten = len - l;
-                        
-            return StringToken(i, p, l, eaten, v);
-        } else if (isstartnum(*p)) {
-            *eaten = len - l;
-                        
-            return NumToken(i, p, l, eaten, v);
-        } else {
-            *eaten = len - l;
-                        
-            return NameToken(i, p, l, eaten, v);
-        }
-    }
-
-    return NULL;
-
-ok2:
-    v->Flags |= PLINC_ATTR_DOEXEC;
-
-ok1:
-    v->Flags |= PLINC_TYPE_NAME;
-    l--;   
-    
-/*ok:*/
-    *eaten = len - l;
-
-    return i;        
-}
-
-
-
-static void *
-NumToken(PlincInterp *i, char *buf, size_t len, size_t *eaten, PlincVal *v)
-{
-    int base, neg = FALSE;
-    char *p = buf;
-    size_t l = len;
-    PlincUInt n;
-    
-    if (*p == '+') {
-        p++; l--;
-    } else if (*p == '-') {
-        p++; l--;
-        neg = TRUE;
-    }
-    
-    if (IntToken(&p, &l, &n, 10)) {
-        if (*p == '#') {
-            base = n; n = 0;
-            p++; l--;
-            if (l) {
-                if (IntToken(&p, &l, &n, base)) {
-                    goto gotint;
-                } else {
-                    return i->syntaxerror;
-                }
-            }
-        } else if ((*p == '.') || (*p == 'e') || (*p == 'E')) {
-        } else {
-            goto gotint;            
-        }
-    }
-
-    return NULL;
-    
-gotint:
-    v->Flags = PLINC_ATTR_LIT | PLINC_TYPE_INT;
-    
-    if (neg) {
-        v->Val.Int = -n;
-    } else {
-        v->Val.Int = n;
-    }
-
-    *eaten += (len - l);
-            
-    return i;
-}
-
-
-
-static int
-IntToken(char **p, size_t *l, PlincInt *n, int base)
-{
-    *n = 0;
-    
-    while (l) {
-        if (isdigit(**p) && (val(**p) < base)) {
-            *n *= base;
-            *n += val(**p);
-            (*p)++; (*l)--;
-        } else {
-            return TRUE;
-        }
-    }
-    
-    return FALSE;
-}
-
-
-static void *
-NameToken(PlincInterp *i, char *buf, size_t len, size_t *eaten, PlincVal *v)
-{            
-    char *ns, *p = buf;
-    size_t nl, l = len;
-
-    v->Flags = PLINC_TYPE_NAME;
-            
-    if (*p == '/') {
-        v->Flags |= PLINC_ATTR_LIT;
-        p++; l--;
-    }
-
-    ns = p; nl = 0;
-    
-    if (l) {
-        while (l && !isendname(*p)) {
-            l--; p++; nl++;
-        }
-        
-        if (l) {
-            *eaten += (len - l);
-            
-            v->Val.Ptr = PlincName(i->Heap, ns, nl);
-            
-            if (v->Val.Ptr) {
-                return i;
-            } else {
-                return i->VMerror;
-            }
-        }
-    }
-
-    return NULL;
-}
-
-
-static void *
-StringToken(PlincInterp *i, char *buf, size_t len, size_t *eaten, PlincVal *v)
-{
-    size_t sl = 0, l = len;
-    char *p = buf, *s;
-    int nest = 1;
-
-    while (l && nest) {
-        if (*p == '(') {
-            nest++; sl++;
-        } else if (*p == ')') {
-            nest--; sl++;
-        } else if (*p == '\\') {
-            p++; l--;
-            if (l) {
-                if (*p == '\n') {
-                    /* nothing */
-                } else if (isoctal(*p)) {
-                    if (l > 1) {
-                        sl++;
-                        if (isoctal(p[1])) {
-                            p++; l--;
-                            if (l > 1) {
-                                if (isoctal(p[1])) {
-                                    p++; l--;
-                                }
-                            } else {
-                                return NULL;
-                            }
-                        }
-                    } else {
-                        return NULL;
-                    }
-                } else {
-                    sl++;
-                }
-            } else {
-                return NULL;
-            }
-        } else {
-            sl++;
-        }
-        
-        p++; l--;
-    }
-    
-    if (nest) {
-        return NULL;
-    } else {
-        sl--;
-        
-        if (sl > PLINC_MAXLEN) {
-            return i->limitcheck;
-        }
-        
-        s = PlincNewString(i->Heap, sl);
-        
-        v->Flags = PLINC_ATTR_LIT | PLINC_TYPE_STRING | sl;
-        v->Val.Ptr = s;
-        
-        p = buf;
-        nest = 1;
-
-        while (nest) {
-            if (*p == '(') {
-                nest++; *s++ = *p;
-            } else if (*p == ')') {
-                nest--;
-                if (nest) {
-                    *s++ = *p;
-                }
-            } else if (*p == '\\') {
-                p++;
-                if (*p == '\n') {
-                    /* nothing */
-                } else if (*p == 'n') {
-                    *s++ = '\n';
-                } else if (*p == 'r') {
-                    *s++ = '\r';
-                } else if (*p == 't') {
-                    *s++ = '\t';
-                } else if (*p == 'b') {
-                    *s++ = '\x8';
-                } else if (*p == 'f') {
-                    *s++ = '\f';
-                } else if (*p == '\\') {
-                    *s++ = *p;
-                } else if (isoctal(*p)) {
-                    *s = val(*p);
-
-                    if (isoctal(p[1])) {
-                        p++;
-                        *s <<= 3;
-                        *s |= val(*p);
-                        
-                        if (isoctal(p[1])) {
-                            p++;
-                            *s <<= 3;
-                            *s |= val(*p);
-                        }
-                    }
-
-                    s++;
-                } else {
-                    *s++ = *p;
-                }
-            } else {
-                *s++ = *p;
-            }
-        
-            p++;
-        }
-
-        *eaten += (len - l);
-
-        return i;
-    }
-}
-#else
 void *
 PlincToken(PlincInterp *i, char *buf, size_t len, size_t *eaten, PlincVal *v)
 {
@@ -368,7 +32,6 @@ PlincToken(PlincInterp *i, char *buf, size_t len, size_t *eaten, PlincVal *v)
     
     return i;
 }
-#endif
 
 
 
@@ -547,12 +210,12 @@ PlincGetToken(PlincInterp *i, PlincFile *f, PlincVal *val)
         case '[':
             v.Flags = PLINC_TYPE_NAME;
             v.Val.Ptr = i->LeftBracket;
-            goto value;
+            break;
 
         case ']':
             v.Flags = PLINC_TYPE_NAME;
             v.Val.Ptr = i->RightBracket;
-            goto value;
+            break;
          
         case '{':
             if (depth) {
@@ -591,7 +254,7 @@ PlincGetToken(PlincInterp *i, PlincFile *f, PlincVal *val)
                     i->ExecStack.Len--;
                     continue;
                 } else {
-                    goto value;
+                    break;
                 }
             } else {
                 r = i->syntaxerror;
@@ -603,7 +266,7 @@ PlincGetToken(PlincInterp *i, PlincFile *f, PlincVal *val)
             if (c == '<') {
                 v.Flags = PLINC_TYPE_NAME;
                 v.Val.Ptr = i->LeftAngleAngle;
-                goto value;
+                break;
 #if 0
             } else if (c == '~') {
                 XXX
@@ -618,7 +281,7 @@ PlincGetToken(PlincInterp *i, PlincFile *f, PlincVal *val)
                 if (r) {
                     goto done;
                 }
-                goto value;
+                break;
             }
             
         case '>':
@@ -632,7 +295,7 @@ PlincGetToken(PlincInterp *i, PlincFile *f, PlincVal *val)
             } else if (c == '>') {
                 v.Flags = PLINC_TYPE_NAME;
                 v.Val.Ptr = i->RightAngleAngle;
-                goto value;
+                break;
             } else {
                 c = PlincUnRead(f, c);
                 if (c) {
@@ -648,7 +311,7 @@ PlincGetToken(PlincInterp *i, PlincFile *f, PlincVal *val)
             if (r) {
                 goto done;
             }
-            goto value;
+            break;
             
         case ')':
             r = i->syntaxerror;
@@ -678,14 +341,14 @@ PlincGetToken(PlincInterp *i, PlincFile *f, PlincVal *val)
                 if (r) {
                     goto done;
                 } else {
-                    goto value;
+                    break;
                 }
             } else {
                 r = PlincGetOther(i, f, &v, c, 1);
                 if (r) {
                     goto done;
                 } else {
-                    goto value;
+                    break;
                 }
             }
             
@@ -694,13 +357,12 @@ PlincGetToken(PlincInterp *i, PlincFile *f, PlincVal *val)
             if (r) {
                 goto done;
             } else {
-                goto value;
+                break;
             }
         }
 
-        return i->invalidaccess;    /* XXX shouldn't happen */
-        
-    value:
+        /* ok, now do something with the value we got */
+
         if (depth) {
             if (!PLINC_OPSTACKROOM(i, 1)) {
                 r = i->stackoverflow;
