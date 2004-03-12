2f0be44fe3d0814f618104534c420c21c728fb89
Ty Sarna <ty@sarna.org>
2004-03-12 00:01:47 +0000

    basic exec is working
    git-svn-id: file:///Users/tsarna/src/SVNREPO/plinc/trunk@250 933abd37-16e6-440f-98ae-168aefc19175

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define PLINC_VERSION_H
 
 #define PLINC_PRODUCT "Plinc"
-#define PLINC_VERSION "Milestone 21"
-#define PLINC_REVISION  (-100+21)
+#define PLINC_VERSION "Milestone 22"
+#define PLINC_REVISION  (-100+22)
 
 #endif /* PLINC_VERSION_H */
