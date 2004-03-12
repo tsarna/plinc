159ae5ae26d8cc8ff197f74355b4ad3316c141fe
Ty Sarna <ty@sarna.org>
2004-03-12 21:26:39 +0000

    error handling logic in exec
    git-svn-id: file:///Users/tsarna/src/SVNREPO/plinc/trunk@255 933abd37-16e6-440f-98ae-168aefc19175

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define PLINC_VERSION_H
 
 #define PLINC_PRODUCT "Plinc"
-#define PLINC_VERSION "Milestone 22"
-#define PLINC_REVISION  (-100+22)
+#define PLINC_VERSION "Milestone 23"
+#define PLINC_REVISION  (-100+23)
 
 #endif /* PLINC_VERSION_H */
